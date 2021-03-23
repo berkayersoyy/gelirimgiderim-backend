93b543c99633f9014dac4c6bf1cb121dd746f12b
Berkay <gelirimgiderimm@gmail.com>
2021-03-23 13:07:19 +0300

    Layer Architecture

---
+++ DataAccess.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataAccess.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

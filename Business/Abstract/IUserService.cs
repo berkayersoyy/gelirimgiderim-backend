951a2d3e38475396a4d3ad19416a34c61c6cb5f5
Berkay <gelirimgiderimm@gmail.com>
2021-03-30 00:59:30 +0300

    user service added

---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Core.Entities.Concrete;
+using Core.Utilities.Results;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        IResult Add(User user);
+        IResult Delete(User user);
+        IDataResult<User> GetByMail(string email);
+    }
+}
---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using Business.Abstract;
+using Business.Constants;
+using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+
+namespace Business.Concrete
+{
+    public class UserManager:IUserService
+    {
+        private IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public IResult Add(User user)
+        {
+            _userDal.Add(user);
+            return new SuccessResult(Messages.UserAdded);
+        }
+
+        public IResult Delete(User user)
+        {
+            _userDal.Delete(user);
+            return new SuccessResult(Messages.UserDeleted);
+        }
+
+        public IDataResult<User> GetByMail(string email)
+        {
+            var user = _userDal.GetAll().SingleOrDefault(u => u.Email == email);
+            return new SuccessDataResult<User>(user,Messages.UserFetchedByMail);
+        }
+    }
+}

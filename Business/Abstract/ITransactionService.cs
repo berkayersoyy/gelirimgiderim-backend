0220adbb4633257c4fa18bc30ca239a9faf8410d
Berkay <gelirimgiderimm@gmail.com>
2021-03-30 00:59:20 +0300

    transaction service added

--- Business/Abstract/ITransactionService.cs
+++ Business/Abstract/ITransactionService.cs
@@ -1,7 +1,15 @@
-﻿namespace Business.Abstract
+﻿using System.Collections.Generic;
+using Core.Utilities.Results;
+using Entities.Concrete;
+
+namespace Business.Abstract
 {
     public interface ITransactionService
     {
-
+        IDataResult<List<Transaction>> GetList();
+        IDataResult<Transaction> Get(Transaction transaction);
+        IResult Add(Transaction transaction);
+        IResult Update(Transaction transaction);
+        IResult Delete(Transaction transaction);
     }
 }
---
+++ Business/Concrete/TransactionManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using DataAccess.Concrete.Firebase;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class TransactionManager:ITransactionService
+    {
+        private ITransactionDal _transactionDal;
+
+        public TransactionManager(ITransactionDal fbTransactionDal)
+        {
+            _transactionDal = fbTransactionDal;
+        }
+
+        public IDataResult<List<Transaction>> GetList()
+        {
+            return new SuccessDataResult<List<Transaction>>(_transactionDal.GetAll(),Messages.TransactionsFetched);
+        }
+
+        public IDataResult<Transaction> Get(Transaction transaction)
+        {
+            return new SuccessDataResult<Transaction>(_transactionDal.Get(transaction),Messages.TransactionFetched);
+        }
+
+        public IResult Add(Transaction transaction)
+        {
+            _transactionDal.Add(transaction);
+            return new SuccessResult(Messages.TransactionAdded);
+        }
+
+        public IResult Update(Transaction transaction)
+        {
+            _transactionDal.Update(transaction);
+            return new SuccessResult(Messages.TransactionUpdated);
+        }
+
+        public IResult Delete(Transaction transaction)
+        {
+            _fbTransactionDal.Delete(transaction);
+            return new SuccessResult(Messages.TransactionDeleted);
+        }
+    }
+}

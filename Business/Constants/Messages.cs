7adf9829e944aa47acc442e97a04208787ece7be
Berkay <gelirimgiderimm@gmail.com>
2021-03-30 00:59:37 +0300

    messages added

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,21 @@
+﻿namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string TransactionsFetched = "Transactions fetched.";
+        public static string TransactionFetched = "Transaction fetched.";
+        public static string TransactionAdded = "Transaction added.";
+        public static string TransactionDeleted = "Transaction deleted.";
+        public static string TransactionUpdated = "Transaction updated.";
+
+        public static string CategoriesFetched = "Categories fetched.";
+        public static string CategoryFetched = "Category fetched.";
+        public static string CategoryAdded = "Category added.";
+        public static string CategoryUpdated = "Category updated.";
+        public static string CategoryDeleted = "Category deleted.";
+
+        public static string UserFetchedByMail = "User fetched by mail.";
+        public static string UserAdded = "User added.";
+        public static string UserDeleted = "User deleted.";
+    }
+}

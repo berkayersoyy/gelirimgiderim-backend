278f8759f624b40b6c7debd1af6b34c34752605f
Berkay <gelirimgiderimm@gmail.com>
2021-03-30 00:59:44 +0300

    ioc container added

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.Firebase;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<FbTransactionDal>().As<ITransactionDal>().SingleInstance();
+            builder.RegisterType<FbCategoryDal>().As<ICategoryDal>().SingleInstance();
+            builder.RegisterType<FbUserDal>().As<IUserDal>().SingleInstance();
+
+            builder.RegisterType<TransactionManager>().As<ITransactionService>().SingleInstance();
+            builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
+        }
+    }
+}

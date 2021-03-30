eff9be1c7c4a52d7185a8f02dedcf9c03c44207d
Berkay <gelirimgiderimm@gmail.com>
2021-03-30 17:03:34 +0300

    aspect interceptor added

---
+++ Core/Utilities/Interceptors/MethodInterception.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Castle.DynamicProxy;
+
+namespace Core.Utilities.Interceptors
+{
+    public abstract class MethodInterception:MethodInterceptionBaseAttribute
+    {
+        public virtual void OnBefore(IInvocation invocation){}
+        public virtual void OnAfter(IInvocation invocation){}
+        public virtual void OnException(IInvocation invocation,System.Exception e){}
+        public virtual void OnSuccess(IInvocation invocation){}
+        public override void Intercept(IInvocation invocation)
+        {
+            var isSuccess = true;
+            OnBefore(invocation);
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception e)
+            {
+                isSuccess = false;
+                OnException(invocation, e);
+                throw;
+            }
+            finally
+            {
+                if (isSuccess)
+                {
+                    OnSuccess(invocation);
+                }
+            }
+            OnAfter(invocation);
+        }
+    }
+}

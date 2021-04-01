ed01fbe77695214e94957a1009f064ac890b4100
Berkay <gelirimgiderimm@gmail.com>
2021-04-02 02:44:10 +0300

    claim entities added

---
+++ Core/Entities/Concrete/OperationClaim.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Entities.Concrete
+{
+    public class OperationClaim
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Core/Entities/Concrete/UserOperationClaim.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities.Concrete
+{
+    public class UserOperationClaim
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string OperationClaimId { get; set; }
+    }
+}

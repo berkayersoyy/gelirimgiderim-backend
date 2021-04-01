78bde7ac8e50a2241764802bee493b5f49e98b1e
Berkay <gelirimgiderimm@gmail.com>
2021-04-02 02:42:39 +0300

    file reader utility added

---
+++ Core/Utilities/FileReader/FileReader.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace Core.Utilities.FileReader
+{
+    public static class FileReader
+    {
+        public static string ReadFileName(string path)
+        {
+            FileInfo fileInfo = new FileInfo(path);
+            return fileInfo.FullName;
+        }
+
+        public static string ReadFileExtension(string path)
+        {
+            FileInfo fileInfo = new FileInfo(path);
+            return fileInfo.Extension;
+        }
+    }
+}

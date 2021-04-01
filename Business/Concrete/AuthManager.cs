fa544360a7843f872370088a34e4395947dcc62b
Berkay <gelirimgiderimm@gmail.com>
2021-04-02 02:44:29 +0300

    auth service added

---
+++ Business/Concrete/AuthManager.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Constants;
+using Core.Constants;
+using Core.Utilities.Results;
+using Core.Utilities.Security.Hashing;
+using Core.Utilities.Security.Jwt;
+using Entities.Dtos;
+using Firebase.Auth;
+using User = Core.Entities.Concrete.User;
+
+namespace Business.Concrete
+{
+    public class AuthManager:IAuthService
+    {
+        private IUserService _userService;
+        private ITokenHelper _tokenHelper;
+
+        public AuthManager(IUserService userService, ITokenHelper tokenHelper)
+        {
+            _userService = userService;
+            _tokenHelper = tokenHelper;
+        }
+
+        public IDataResult<User> Register(UserForRegisterDto userForRegisterDto)
+        {
+            byte[] passwordHash, passwordSalt;
+            HashingHelper.CreatePasswordHash(userForRegisterDto.Password,out passwordHash,out passwordSalt);
+            var user = new User
+            {
+                Email = userForRegisterDto.Email,
+                FirstName = userForRegisterDto.FirstName,
+                LastName = userForRegisterDto.LastName,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                Status = true
+            };
+            _userService.Add(user);
+            return new SuccessDataResult<User>(user,Messages.UserRegistered);
+        }
+
+        public IDataResult<User> Login(UserForLoginDto userForLoginDto)
+        {
+            var userToCheck = _userService.GetByMail(userForLoginDto.Email);
+            if (userToCheck==null)
+            {
+                return new ErrorDataResult<User>(Messages.UserNotFound);
+            }
+
+            if (!HashingHelper.VerifyPasswordHash(userForLoginDto.Password,userToCheck.Data.PasswordHash,userToCheck.Data.PasswordHash))
+            {
+                return new ErrorDataResult<User>(Messages.WrongPassword);
+            }
+            return new SuccessDataResult<User>(userToCheck.Data,Messages.SuccessfulLogin);
+        }
+
+        public IResult UserExists(string email)
+        {
+            if (_userService.GetByMail(email).Data!=null)
+            {
+                return new ErrorResult(Messages.UserExists);
+            }
+            return new SuccessResult();
+        }
+
+        public IDataResult<AccessToken> CreateAccessToken(User user)
+        {
+            var claims = _userService.GetClaims(user);
+            var accessToken = _tokenHelper.CreateToken(user, claims.Data);
+            return new SuccessDataResult<AccessToken>(accessToken,Messages.AccessTokenCreated);
+        }
+    }
+}

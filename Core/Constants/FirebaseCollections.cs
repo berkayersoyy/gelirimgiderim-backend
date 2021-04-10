461d5970f2fa659fbd1d812b21e7e07ee5a51ab9
Berkay <gelirimgiderimm@gmail.com>
2021-04-10 19:16:38 +0300

    new collections and paths added

--- Core/Constants/FirebaseCollections.cs
+++ Core/Constants/FirebaseCollections.cs
@@ -5,5 +5,9 @@
         public static string Users = "users";
         public static string Transactions = "transactions";
         public static string Categories = "categories";
+        public static string OperationClaims = "operationClaims";
+        public static string UserOperationClaims = "userOperationClaims";
+        public static string Rooms = "rooms";
+        public static string UserRooms = "userRooms";
     }
 }

41c893346f3fee7b9af0f6359869f84caab2b425
Berkay <gelirimgiderimm@gmail.com>
2021-04-14 03:02:04 +0300

    core ioc container added

---
+++ Core/DependencyResolvers/CoreModule.cs
@@ -0,0 +1,14 @@
+﻿using Core.Utilities.IoC;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+
+namespace Core.DependencyResolvers
+{
+    public class CoreModule:ICoreModule
+    {
+        public void Load(IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor,HttpContextAccessor>();
+        }
+    }
+}

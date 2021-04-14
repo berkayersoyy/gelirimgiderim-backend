081a909c4cef5175705aca05ec9a6dfd0c56a32c
Berkay <gelirimgiderimm@gmail.com>
2021-04-14 03:00:29 +0300

    rooms controller added

---
+++ WebAPI/Controllers/RoomsController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Core.Entities.Concrete;
+using Entities.Concrete;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        private IRoomService _roomService;
+
+        public RoomsController(IRoomService roomService)
+        {
+            _roomService = roomService;
+        }
+
+        [HttpGet("getall")]
+        public ActionResult GetUserRooms(User user)
+        {
+            var result = _roomService.GetUserRooms(user);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("getusersexist")]
+        public ActionResult GetUsersExistInRoom(Room room)
+        {
+            var result = _roomService.GetUsersExistInRoom(room);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+        [HttpPost("add")]
+        public ActionResult Add(Room room)
+        {
+            var result = _roomService.Add(room);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpPost("update")]
+        public ActionResult Update(Room room)
+        {
+            var result = _roomService.Update(room);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpPost("delete")]
+        public ActionResult Delete(Room room)
+        {
+            var result = _roomService.Delete(room);
+            if (result.Success)
+            {
+                Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+    }
+}

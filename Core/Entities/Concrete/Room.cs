e72d03d25f9e61b6d768a7dad07774adce9dddee
Berkay <gelirimgiderimm@gmail.com>
2021-04-16 02:46:59 +0300

    room location changed entities to core entities because of preventing circular dependency

---
+++ Core/Entities/Concrete/Room.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities.Abstract;
+using Google.Cloud.Firestore;
+
+namespace Core.Entities.Concrete
+{
+    [FirestoreData]
+    public class Room:IEntity
+    {
+        [FirestoreProperty]
+        public string Id { get; set; }
+        [FirestoreProperty]
+        public string Name { get; set; }
+
+    }
+}

e21685fe3fe892d40b33b64228ef1a58ef2baac9
Berkay <gelirimgiderimm@gmail.com>
2021-04-18 02:29:23 +0300

    dto created for user to join room

---
+++ Entities/Dtos/UserForJoinRoom.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities.Concrete;
+
+namespace Entities.Dtos
+{
+    public class UserForJoinRoom
+    {
+        public User User { get; set; }
+        public string Invitation { get; set; }
+    }
+}

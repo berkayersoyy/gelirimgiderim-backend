825063b04f594df773008eb9a48094c39163ce7d
Berkay <gelirimgiderimm@gmail.com>
2021-04-18 02:27:37 +0300

    invitation updated

--- Core/Entities/Concrete/Invitation.cs
+++ Core/Entities/Concrete/Invitation.cs
@@ -1,11 +1,17 @@
 ﻿using Core.Entities.Abstract;
+using Google.Cloud.Firestore;
 
 namespace Core.Entities.Concrete
 {
+    [FirestoreData]
     public class Invitation:IEntity
     {
+        [FirestoreProperty]
         public string Id { get; set; }
+        [FirestoreProperty]
         public string RoomId { get; set; }
+        [FirestoreProperty]
         public string Code { get; set; }
+        ///TODO Expire time will be added
     }
 }

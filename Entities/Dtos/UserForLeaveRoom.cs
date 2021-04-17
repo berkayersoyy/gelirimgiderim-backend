441eb40a4972cf3a5ddcae761b47c8daa292e0ff
Berkay <gelirimgiderimm@gmail.com>
2021-04-18 02:29:31 +0300

    dto created for user to leave room

---
+++ Entities/Dtos/UserForLeaveRoom.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities.Concrete;
+
+namespace Entities.Dtos
+{
+    public class UserForLeaveRoom
+    {
+        public User User { get; set; }
+        public Room Room { get; set; }
+    }
+}

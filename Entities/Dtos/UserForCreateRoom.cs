8d650530bbb2a3529ee445e600590f742552acb9
Berkay <gelirimgiderimm@gmail.com>
2021-04-18 02:29:13 +0300

    dto added for user to create room

---
+++ Entities/Dtos/UserForCreateRoom.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities.Abstract;
+using Core.Entities.Concrete;
+
+namespace Entities.Dtos
+{
+    public class UserForCreateRoom:IDto
+    {
+        public User User { get; set; }
+        public Room Room { get; set; }
+    }
+}

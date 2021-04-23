37b71c41fdf2bccf4eb02c5b5edd8ae4197af803
berkay ersoy <berkay.ersoy.007@gmail.com>
2021-04-23 18:06:35 +0300

    new messages added

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,34 +1,51 @@
-﻿using System;
+using System;
 
 namespace Business.Constants
 {
-    public static class Messages
-    {
-        public static string TransactionsFetched = "Transactions fetched.";
-        public static string TransactionFetched = "Transaction fetched.";
-        public static string TransactionAdded = "Transaction added.";
-        public static string TransactionDeleted = "Transaction deleted.";
-        public static string TransactionUpdated = "Transaction updated.";
+  public static class Messages
+  {
+    public static string TransactionsFetched = "Transactions fetched.";
+    public static string TransactionFetched = "Transaction fetched.";
+    public static string TransactionAdded = "Transaction added.";
+    public static string TransactionDeleted = "Transaction deleted.";
+    public static string TransactionUpdated = "Transaction updated.";
 
-        public static string CategoriesFetched = "Categories fetched.";
-        public static string CategoryFetched = "Category fetched.";
-        public static string CategoryAdded = "Category added.";
-        public static string CategoryUpdated = "Category updated.";
-        public static string CategoryDeleted = "Category deleted.";
+    public static string CategoriesFetched = "Categories fetched.";
+    public static string CategoryFetched = "Category fetched.";
+    public static string CategoryAdded = "Category added.";
+    public static string CategoryUpdated = "Category updated.";
+    public static string CategoryDeleted = "Category deleted.";
 
-        public static string UserFetchedByMail = "User fetched by mail.";
-        public static string UserAdded = "User added.";
-        public static string UserDeleted = "User deleted.";
+    public static string UserFetchedByMail = "User fetched by mail.";
+    public static string UserAdded = "User added.";
+    public static string UserDeleted = "User deleted.";
 
-        public static string UserRegistered = "User registered.";
-        public static string UserNotFound = "User not found.";
-        public static string WrongPassword = "Wrong password.";
-        public static string SuccessfulLogin = "Login successfully.";
-        public static string UserExists = "User exists.";
-        public static string AccessTokenCreated = "Access token created.";
-        public static string UserClaimsFetched = "User claims fetched.";
-        public static string UserRoomsFetched = "User rooms fetched.";
+    public static string UserRegistered = "User registered.";
+    public static string UserNotFound = "User not found.";
+    public static string WrongPassword = "Wrong password.";
+    public static string SuccessfulLogin = "Login successfully.";
+    public static string UserExists = "User exists.";
+    public static string AccessTokenCreated = "Access token created.";
+    public static string UserClaimsFetched = "User claims fetched.";
+    public static string UserRoomsFetched = "User rooms fetched.";
+    public static string RoomsFetched = "Rooms fetched.";
+    public static string RoomFetched = "Room fetched.";
+    public static string UsersExistInRoomFetched = "Users fetched whose existing in room.";
+    public static string InvitationCreated = "Invitation created.";
+    public static string InvitationExists = "Invitation exists.";
+    public static string InvitationFetched = "Invitation fetched.";
+    public static string InvitationDeleted = "Invitation deleted.";
+    public static string InvitationNotFound = "Invitation not found.";
+    public static string JoinRoomSuccessful = "Joined room successfuly.";
+    public static string LeaveRoomSuccessful = "Room left successfuly.";
+    public static string RoomCreated = "Room created.";
+    public static string RoomDeleted = "Room deleted.";
+    public static string RoomUpdated = "Room updated.";
+    public static string UserFetchedById = "User fetched by id.";
+    public static string CurrentUserFetched = "Current user fetched.";
 
-        public static string AuthorizationDenied = "Authorization denied.";
-    }
+
+
+    public static string AuthorizationDenied = "Authorization denied.";
+  }
 }

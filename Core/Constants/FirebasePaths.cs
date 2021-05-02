86e254daba89262808c9e2848a0ca240fec61476
berkay ersoy <ersoyberkay_fb@hotmail.com>
2021-05-02 06:41:03 +0300

    02052021

--- Core/Constants/FirebasePaths.cs
+++ Core/Constants/FirebasePaths.cs
@@ -3,7 +3,8 @@
     public static class FirebasePaths
     {
         public static string ProjectId = "gelirimgiderim-d0b53";
-        public static string FilePath = @"C:\Users\BERKAY\source\repos\GelirimGiderim\Core\DataAccess\FirebaseDatabase\gelirimgiderim-d0b53-firebase-adminsdk-e6whg-67c64a2c74.json";
+        public static string FilePathForLocal = @"C:\Users\BERKAY\source\repos\GelirimGiderim\Core\DataAccess\FirebaseDatabase\gelirimgiderim-d0b53-firebase-adminsdk-e6whg-67c64a2c74.json";
+        public static string FilePath = "Database//FirebaseDatabase//gelirimgiderim-d0b53-firebase-adminsdk-e6whg-67c64a2c74.json";
         public static string ApiKey = "AIzaSyCoX8qD3H53-IzUv5h97-W-FlF47qIbPug";
         public static string AppBucket = "gelirimgiderim-d0b53.appspot.com";
     }

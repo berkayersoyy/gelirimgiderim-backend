fd11836db006c2164088b7a098bb99ebd16f0f13
berkay ersoy <ersoyberkay_fb@hotmail.com>
2021-05-16 02:08:49 +0300

    firebase upload updated.

--- Core/DataAccess/FirebaseStorage/FirebaseStorageRepositoryBase.cs
+++ Core/DataAccess/FirebaseStorage/FirebaseStorageRepositoryBase.cs
@@ -6,62 +6,43 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Core.Constants;
-using Core.Utilities.FileReader;
 using Firebase.Auth;
 
 
 namespace Core.DataAccess.FirebaseStorage
 {
-    public class FirebaseStorageRepositoryBase<T>:IStorageRepository<T>
+    public class FirebaseStorageRepositoryBase<T> : IStorageRepository<T>
     {
-        public async Task Upload()
-        {
-            var stream = new MemoryStream(File.ReadAllBytes(@"C:\Users\BERKAY\Desktop\1.mp4"));
-            var auth = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyCoX8qD3H53-IzUv5h97-W-FlF47qIbPug"));
-            var a = await auth.SignInWithEmailAndPasswordAsync("afafa@hotmail.com", "123456");
-            var cancellation = new CancellationTokenSource();
-            var upload = new Firebase.Storage.FirebaseStorage("gelirimgiderim-d0b53.appspot.com").Child("images").
-                Child("hehehehehe").
-                PutAsync(stream,cancellation.Token);
-            var url = await upload;
-        }
-
-        public async void Upload(string path, string collection, string email, string password)
+        public async Task<string> Upload(string path, string collection)
         {
             var stream = new MemoryStream(File.ReadAllBytes(path));
             var auth = new FirebaseAuthProvider(new FirebaseConfig(FirebasePaths.ApiKey));
-            var login = await auth.SignInWithEmailAndPasswordAsync(email, password);
-            var result = new Firebase.Storage.FirebaseStorage(FirebasePaths.AppBucket)
+            var login = await auth.SignInWithEmailAndPasswordAsync("forusestorage@hotmail.com", "x03121998X+");
+            var cancellation = new CancellationTokenSource();
+            FileInfo info = new FileInfo(path);
+            var upload = new Firebase.Storage.FirebaseStorage(FirebasePaths.AppBucket)
+                .Child("images")
                 .Child(collection)
-                .Child(FileReader.ReadFileName(path));
+                .Child(info.Name)
+                .PutAsync(stream, cancellation.Token);
+            return await upload;
         }
 
-        public async void Delete(string path, string collection, string email, string password)
+        public async void Delete(string collection, string fileName)
         {
             var auth = new FirebaseAuthProvider(new FirebaseConfig(FirebasePaths.ApiKey));
-            var login = await auth.SignInWithEmailAndPasswordAsync(email, password);
-            var result = new Firebase.Storage.FirebaseStorage(FirebasePaths.AppBucket).Child(collection).Child(path)
+            var login = await auth.SignInWithEmailAndPasswordAsync("forusestorage@hotmail.com", "x03121998X+");
+            var result = new Firebase.Storage.FirebaseStorage(FirebasePaths.AppBucket).Child("images").Child(collection).Child(fileName)
                 .DeleteAsync();
         }
 
-        public async Task<string> Get(string path, string collection,string email, string password)
+        public async Task<string> Get(string fileName, string collection)
         {
             var auth = new FirebaseAuthProvider(new FirebaseConfig(FirebasePaths.ApiKey));
-            var login = await auth.SignInWithEmailAndPasswordAsync(email, password);
-            var result = new Firebase.Storage.FirebaseStorage(FirebasePaths.AppBucket).Child(collection).Child(path)
+            var login = await auth.SignInWithEmailAndPasswordAsync("forusestorage@hotmail.com", "x03121998X+");
+            var result = new Firebase.Storage.FirebaseStorage(FirebasePaths.AppBucket).Child("images").Child(collection).Child(fileName)
                 .GetDownloadUrlAsync();
             return await result;
-        }
-
-        public async Task<List<string>> GetAll(string collection,string email,string password)
-        {
-            List<string> urlList = new List<string>();
-            var auth = new FirebaseAuthProvider(new FirebaseConfig(FirebasePaths.ApiKey));
-            var login = await auth.SignInWithEmailAndPasswordAsync(email, password);
-            var result = new Firebase.Storage.FirebaseStorage(FirebasePaths.AppBucket).Child(collection).GetDownloadUrlAsync();
-            //TODO auth managing need to be added
-            //TODO need to test
-            throw new NotImplementedException();
         }
     }
 }
--- Core/DataAccess/IStorageRepository.cs
+++ Core/DataAccess/IStorageRepository.cs
@@ -5,10 +5,9 @@ namespace Core.DataAccess
 {
     public interface IStorageRepository<T>
     {
-        void Upload(string path, string collection,string email, string password);
-        void Delete(string path, string collection, string email, string password);
-        Task<string> Get(string path, string collection,string email, string password);
-        Task<List<string>> GetAll(string collection, string email, string password);
+        Task<string> Upload(string path, string collection);
+        void Delete(string collection, string fileName);
+        Task<string> Get(string collection, string fileName);
 
     }
 }

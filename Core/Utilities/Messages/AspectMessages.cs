5fbf3197acd18e29c91bd22c7086c5b11ecfac14
berkay ersoy <ersoyberkay_fb@hotmail.com>
2021-05-17 13:51:42 +0300

    messages updated

--- Core/Utilities/Messages/AspectMessages.cs
+++ Core/Utilities/Messages/AspectMessages.cs
@@ -1,7 +1,10 @@
-﻿namespace Core.Utilities.Messages
+﻿using System.Runtime.CompilerServices;
+
+namespace Core.Utilities.Messages
 {
     public static class AspectMessages
     {
         public static string WrongValidationType = "Wrong validation type.";
+        public static string WrongLoggerType = "Wrong logger type.";
     }
 }
